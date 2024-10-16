318c6b09e54d766830eda584c952e03cfbc5b63a
Zasmooncat <zasmomoxipol@gmail.com>
2024-10-16 20:55:40 +0000

    retoques finales

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand d-flex" href="#">React Landpage</a>
+        <a className="navbar-brand d-flex" href="#">React Landing Page</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
